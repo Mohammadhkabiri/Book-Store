eeaf49dcebd66162b5cebc642408cf1601f98f17
Mohammad <110092962+Mohammadhkabiri@users.noreply.github.com>
2025-05-30 02:59:44 +0330

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const fs = require("fs");
-const { dirname } = require("path");
 
 const app = express();
 
@@ -8,7 +7,11 @@ app.use(express.json());
 
 const books = JSON.parse(fs.readFileSync(`${__dirname}/books.json`));
 
-app.get("/api/v1/books", (req, res) => {
+const is_Id_validate = (id) => {
+  return books.some((book) => book.id === id);
+};
+
+const get_all_book = (req, res) => {
   res.status(200).json({
     status: "success",
     result: books.length,
@@ -16,15 +19,38 @@ app.get("/api/v1/books", (req, res) => {
       books,
     },
   });
-});
+};
 
+const get_book = (req, res) => {
+  const id = req.params.id * 1;
+  const book = books.find((el) => el.id === id);
 
-app.post("/api/v1/books", (req, res) => {
+  if (!is_Id_validate(id)) {
+    return res.status(404).json({
+      status: "fail",
+      message: "invalid ID",
+    });
+  }
+  res.status(200).json({
+    status: "success",
+    data: {
+      book,
+    },
+  });
+};
+
+const add_book = (req, res) => {
   const new_id = books[books.length - 1].id + 1;
   const new_book = Object.assign({ id: new_id }, req.body);
 
   books.push(new_book);
   fs.writeFile(`${__dirname}/books`, JSON.stringify(books), (err) => {
+    if (err) {
+      return res.status(500).json({
+        status: "fail",
+        message: "fali to add book",
+      });
+    }
     res.status(201).json({
       status: "success",
       data: {
@@ -32,33 +58,9 @@ app.post("/api/v1/books", (req, res) => {
       },
     });
   });
-});
+};
 
-
-app.get("/api/v1/books/:id", (req, res) => {
-  const id = req.params.id * 1;
-
-   if (!book) {
-    return res.status(404).json({
-      status: "fail",
-      message: "invalid ID",
-    });
-  }
-
-const book  = books.find(el => el.id === id);
-
-  console.log(req.params);
-  res.status(200).json({
-    status: "success",
-    data : {
-      book
-    }
-  });
-});
-
-
-
-app.patch("/api/v1/books/:id", (req, res) => {
+const upgrade_book_info = (req, res) => {
   const id = req.params.id * 1;
   if (!is_Id_validate(id)) {
     return res.status(404).json({
@@ -69,7 +71,7 @@ app.patch("/api/v1/books/:id", (req, res) => {
 
   const book_index = books.findIndex((el) => el.id === id);
   books[book_index] = { ...books[book_index], ...req.body };
-  fs.writeFile(`${__dirname}/books.json`,JSON.stringify(books), (err) => {
+  fs.writeFile(`${__dirname}/books.json`, JSON.stringify(books), (err) => {
     if (err) {
       return res.status(500).json({
         status: "fail",
@@ -83,38 +85,49 @@ app.patch("/api/v1/books/:id", (req, res) => {
       },
     });
   });
-});
+};
 
-
-app.delete("/api/v1/books/:id", (req, res) => {
+const delete_book = (req, res) => {
   const id = req.params.id * 1;
-  
+
   if (!is_Id_validate(id)) {
     return res.status(404).json({
       status: "fail",
-      message: "Book with this ID not found"
+      message: "Book with this ID not found",
     });
   }
 
   const book_index = books.findIndex((el) => el.id === id);
-books.splice(book_index, 1);
+  books.splice(book_index, 1);
 
   fs.writeFile(`${__dirname}/books.json`, JSON.stringify(books), (err) => {
     if (err) {
       return res.status(500).json({
         status: "error",
-        message: "Could not delete the book from database"
+        message: "Could not delete the book from database",
       });
     }
 
     res.status(204).json({
       status: "success",
       data: {
-        book: null
-      }
+        book: null,
+      },
     });
   });
-});
+};
+
+app
+.route("/api/v1/books")
+.get(get_all_book)
+.post(add_book);
+
+
+app
+  .route("/api/v1/books/:id")
+  .get(get_book)
+  .patch(upgrade_book_info)
+  .delete(delete_book);
 
 const port = 3000;
 
