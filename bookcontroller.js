b0e440a259ca4a65e8271058afbb98842c98d4d3
Mohammad <110092962+Mohammadhkabiri@users.noreply.github.com>
2025-05-31 18:32:09 +0330

    CRUD from database

--- bookcontroller.js
+++ bookcontroller.js
@@ -1,16 +1,5 @@
 const fs = require("fs");
-const Book  = require("./Book.model");
-const books = JSON.parse(fs.readFileSync(`${__dirname}/books.json`));
-
-exports.is_book_id_valid = (req, res, next, val) => {
-  if (req.params.id * 1 > books.length || req.params.id < 0) {
-    return res.status(404).json({
-      status: "Fail",
-      message: "Invalid ID",
-    });
-  }
-  next();
-};
+const Book = require("./Book.model");
 
 exports.check_body = (req, res, next) => {
   if (!req.body.title || !req.body.price) {
@@ -22,74 +11,83 @@ exports.check_body = (req, res, next) => {
   next();
 };
 
-exports.get_all_book = (req, res) => {
-  res.status(200).json({
-    status: "success",
-    result: books.length,
-    data: { books },
-  });
-};
-
-exports.get_book = (req, res) => {
-  const id = req.params.id * 1;
-  const book = books.find((el) => el.id === id);
-  res.status(200).json({
-    status: "success",
-    data: { book },
-  });
-};
-
-exports.add_book = (req, res) => {
-  const new_id = books[books.length - 1].id + 1;
-  const new_book = Object.assign({ id: new_id }, req.body);
-
-  books.push(new_book);
-  fs.writeFile(`${__dirname}/books.json`, JSON.stringify(books), (err) => {
-    if (err) {
-      return res.status(500).json({
-        status: "fail",
-        message: "failed to add book",
-      });
-    }
-    res.status(201).json({
-      status: "success",
-      data: { book: new_book },
-    });
-  });
-};
-
-exports.upgrade_book_info = (req, res) => {
-  const id = req.params.id * 1;
-
-  const book_index = books.findIndex((el) => el.id === id);
-  books[book_index] = { ...books[book_index], ...req.body };
-  fs.writeFile(`${__dirname}/books.json`, JSON.stringify(books), (err) => {
-    if (err) {
-      return res.status(500).json({
-        status: "fail",
-        message: "failed to update book",
-      });
-    }
+exports.get_all_book = async (req, res) => {
+  try {
+    const books = await Book.find();
     res.status(200).json({
       status: "success",
-      data: { book: books[book_index] },
+      result: books.length,
+      data: books,
     });
-  });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
 };
 
-exports.delete_book = (req, res) => {
-  const id = req.params.id * 1;
+exports.get_book = async (req, res) => {
+  try {
+    const book = await books.findById(req.params.id);
+    res.status(200).json({
+      status: "success",
+      data: book,
+    });
+  } catch {
+    res.status(404).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
+};
 
-  const book_index = books.findIndex((el) => el.id === id);
-  books.splice(book_index, 1);
+exports.add_book = async (req, res) => {
+  try {
+    const NewBook = await Book.create(req.body);
+    res.status(201).json({
+      status: "success",
+      data: NewBook,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
+};
 
-  fs.writeFile(`${__dirname}/books.json`, JSON.stringify(books), (err) => {
-    if (err) {
-      return res.status(500).json({
-        status: "error",
-        message: "Could not delete the book",
-      });
-    }
-    res.status(204).end();
-  });
+exports.upgrade_book_info = async (req, res) => {
+  try {
+    const book = await Book.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    res.status(200).json({
+      status: "success",
+      data: {
+        book,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
+};
+
+exports.delete_book = async (req, res) => {
+  try {
+    await Book.findOneAndDelete(req.params.id);
+    res.status(204).json({
+      status: "success",
+      data: null,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "Fail",
+      message: err.message,
+    });
+  }
 };
