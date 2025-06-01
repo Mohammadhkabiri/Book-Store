d48c2d54c6a5408d3094c8843a56a374f8b301ce
Mohammad <110092962+Mohammadhkabiri@users.noreply.github.com>
2025-06-01 22:03:32 +0330

    Delete server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-const { default: mongoose } = require("mongoose");
-const app = require("./app");
-const port = 3000;
-app.listen(port, () => {
-  console.log(`app running on port ${port}`);
-});
-
-
